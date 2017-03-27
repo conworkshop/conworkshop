1b6ba36cadc347f523699a84bd2d15162a9129be
unleashy <lakeavenger@gmail.com>
2017-03-26 23:29:01 -0300

    Put this back because we actually need it

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,11 @@
 require_relative 'boot'
 
-require 'rails'
+require 'rails/all'
 require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_view/railtie'
+require 'action_mailer/railtie'
+require 'active_job/railtie'
 require 'rails/test_unit/railtie'
 require 'sprockets/railtie'
 
