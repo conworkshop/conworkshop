e6c7b8034d5c47514d03976abef1abceb73f47de
Fleur Budek <Fleur Budek>
2017-04-02 16:56:22 +0100

    start i18n of views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+  def set_locale
+    # TODO: = current_user.try(:locale) || I18n.default_locale
+    I18n.locale = I18n.default_locale
+  end
 end
