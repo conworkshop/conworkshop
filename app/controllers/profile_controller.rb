4dd3cc9c41cce07c53d99bb850140219ac2985a8
Fleur Budek <Fleur Budek>
2017-04-03 13:31:51 +0100

    started adding edit profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,6 +1,12 @@
 class ProfileController < ApplicationController
+  before_action :authenticate_user!, except: [:show]
+
   def show
     @profile_user = User.find_by_username(params[:id])
     render 'profile/show'
+  end
+
+  def edit
+    render 'profile/edit'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  # Profile
+  get 'user/:id', to: 'profile#show', constraints: { id: %r{[^/]+} }, as: :profile_show
+  get 'profile/edit', to: 'profile#edit', as: :profile_edit
+
+  # Static pages
   get 'about', to: 'static#about', as: :page_static_about
-  get 'profile/:id', to: 'profile#show', constraints: { id: %r{[^/]+} }
+
+  # Root
   root 'static#about'
+
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
