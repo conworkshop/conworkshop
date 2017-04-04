fe3b7c3074ca241437e634b4e3e4318ca26d5c6f
Fleur Budek <Fleur Budek>
2017-04-04 12:14:41 +0100

    add user handles and feed view

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 class StaticController < ApplicationController
-  def about
+  def about; end
+
+  def feed
+    redirect_to page_static_about_path unless user_signed_in?
   end
 end
