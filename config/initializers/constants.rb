8517a2cdf977a8a36ef98ccd3af52510a83d394d
unleashy <lakeavenger@gmail.com>
2017-04-12 22:24:54 -0300

    Use string keys (sadface)

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
 # temp place to put constants
 
 module CWS
   module Globals
-    GENDER_CODES = { M: 'Male', F: 'Female', O: 'Other', C: 'Cyborg' }.freeze
+    GENDER_CODES = { 'M' => 'Male', 'F' => 'Female', 'O' => 'Other', 'C' => 'Cyborg' }.freeze
 
     def self.gender_codes
       GENDER_CODES
