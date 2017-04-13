7f2fc80134de72c968f505295277ace93fb86e05
unleashy <lakeavenger@gmail.com>
2017-04-12 22:25:12 -0300

    Add validation for Profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,8 +4,12 @@ class Profile < ApplicationRecord
 
   belongs_to :user
 
+  validates :country, inclusion: { in: ISO3166::Country.codes, allow_blank: true }
+  validates :gender, inclusion: { in: CWS::Globals.gender_codes, allow_blank: true }
+  validates :about, length: { maximum: 1000, allow_blank: true }
+
   def display_gender
-    gender.present? ? CWS::Globals.gender_codes[gender.to_sym] : 'Not stated'
+    gender.present? ? CWS::Globals.gender_codes[gender] : 'Not stated'
   end
 
   def display_country
