0c765e0701a2cd031b3bfdd11eb038a674678ba4
unleashy <lakeavenger@gmail.com>
2017-04-15 20:11:57 -0300

    Add RouteAccessControl model

---
+++ app/models/route_access_control.rb
@@ -0,0 +1,2 @@
+class RouteAccessControl < ApplicationRecord
+end
---
+++ db/migrate/20170415230307_create_route_access_controls.rb
@@ -0,0 +1,12 @@
+class CreateRouteAccessControls < ActiveRecord::Migration[5.0]
+  def change
+    create_table :route_access_controls do |t|
+      t.string :route
+      t.integer :level
+
+      t.timestamps
+    end
+
+    add_index :route_access_controls, :route, unique: true
+  end
+end
