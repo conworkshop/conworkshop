d77b1125d1fb78781f172b3a7940430e9899d8c1
unleashy <lakeavenger@gmail.com>
2017-05-19 21:52:51 -0300

    Add a testamajig

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,4 +55,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
 end
---
+++ spec/routing/account_route_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'routing to account' do
+  describe 'GET /account' do
+    it 'routes to account#edit' do
+      expect(get: '/account').to route_to(controller: 'account', action: 'edit')
+    end
+  end
+
+  describe 'PATCH /account' do
+    it 'routes to account#update' do
+      expect(patch: '/account').to route_to(controller: 'account', action: 'update')
+    end
+  end
+end
