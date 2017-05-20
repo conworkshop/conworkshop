5f65362885ed5cec27dedcb5ef42c06b74f47a3b
unleashy <lakeavenger@gmail.com>
2017-05-19 21:28:18 -0300

    Use Faker for creating testamajigs

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :development, :test do
   gem 'rspec'
   gem 'rspec-rails', '~> 3.5'
   gem 'factory_girl_rails', '~> 4.0'
+  gem 'faker'
 end
 
 group :development do
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    email 'user@example.com'
-    password 'spoopy'
+    email { Faker::Internet.email }
+    password '0123456789'
     sequence(:username) { |n| "user#{n}" }
     group 'R'
   end
