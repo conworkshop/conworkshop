8439b2962ba11088a12603066ae921eaa94929a3
unleashy <lakeavenger@gmail.com>
2017-05-25 17:46:11 -0300

    Delete all before seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+RouteAccessControl.delete_all
 RouteAccessControl.create([
   { route: 'static#feed',                       level: -1 },
   { route: 'static#about',                      level: -1 },
