bb903b6f7fda8e39103139c1c35d0ddac3dffc62
unleashy <lakeavenger@gmail.com>
2017-05-25 18:10:19 -0300

    Require RSpec support properly

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,12 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'support/factory_girl'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+Dir.glob(File.join(File.expand_path('..', __FILE__), 'support', '**', '*.rb')) { |file| require file }
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,5 +1,3 @@
-require 'factory_girl'
-
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 end
