50ab1f71ce155058f0fd9d2973f19618fdeca19e
Fleur Budek <fleurbudek@gmail.com>
2017-06-15 00:43:57 +0100

    add clan controller and newview

---
+++ app/controllers/clans_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+class LanguagesController < ApplicationController
+  before_action :authenticate_user!
+
+  def new; end
+  def create; end
+  def index; end
+  def edit; end
+  def update; end
+  def show; end
+  def destroy; end
+end
