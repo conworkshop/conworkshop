d8231edad96a180bbe5d13a6d406f700543808a8
unleashy <lakeavenger@gmail.com>
2017-07-07 23:46:05 -0300

    Move this shit to ImageUploader

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,19 +1,4 @@
 # frozen_string_literal: true
 class AvatarUploader < ImageUploader
   process resize_to_fill: [50, 50]
-
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}"
-  end
-
-  def filename
-    "#{secure_token}.#{file.extension}" if original_filename.present?
-  end
-
-  protected
-
-  def secure_token
-    var = :"@#{mounted_as}_secure_token"
-    model.instance_variable_get(var) || model.instance_variable_set(var, SecureRandom.uuid)
-  end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,9 +16,24 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Set gaussian blur to optimize the image
   process gaussian_blur: 0.05
 
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}"
+  end
+
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
     %w(jpg jpeg gif png)
+  end
+
+  protected
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) || model.instance_variable_set(var, SecureRandom.uuid)
   end
 end
