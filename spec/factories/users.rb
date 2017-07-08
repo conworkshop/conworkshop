167e41bed966f5f3084ec8ae5dbea87f4ce14a7e
unleashy <lakeavenger@gmail.com>
2017-07-07 22:54:54 -0300

    Remove faker dependency

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    email { Faker::Internet.email }
+    email { 'cool@email.com' }
     password '0123456789'
     sequence(:username) { |n| "user#{n}" }
     group 'R'
