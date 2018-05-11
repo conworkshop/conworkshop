cea7b3b6ebb42471d9b50304cfed5e774234b2c5
fleurbheara <fleurbudek@gmail.com>
2018-05-11 21:59:24 +0100

    various, give up trying to name it all just look for yourself lol

--- app/helpers/view_helper.rb
+++ app/helpers/view_helper.rb
@@ -2,19 +2,19 @@
 
 module ViewHelper
   def left_col(options={}, &block)
-    options = options.reverse_merge(class: '', width: 3, padding: 4)
+    options = options.reverse_merge(class: '', width: 3, padding: 'p-4')
     raw tag.div capture(&block), class: "col-xs-12 col-md-#{options[:width]}"\
-                                                 " p-#{options[:padding]} #{options[:class]}"
+                                                 " #{options[:padding]} #{options[:class]}"
   end
 
   def right_col(options={}, &block)
-    options = options.reverse_merge(class: '', width: 9, padding: 4)
+    options = options.reverse_merge(class: '', width: 9, padding: 'p-4')
     raw tag.div capture(&block), class: "col-xs-12 col-md-#{options[:width]}"\
-                                                 " p-#{options[:padding]} #{options[:class]}"
+                                                 " #{options[:padding]} #{options[:class]}"
   end
 
   def full_width(options={}, &block)
-    options = options.reverse_merge(class: '', padding: 4)
-    raw tag.div capture(&block), class: "col-12 p-#{options[:padding]} #{options[:class]}"
+    options = options.reverse_merge(class: '', padding: 'p-4')
+    raw tag.div capture(&block), class: "col-12 #{options[:padding]} #{options[:class]}"
   end
 end
